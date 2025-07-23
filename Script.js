30f3a7c3cf87f01b74bda3175703cc74f6cbf989
motivstile <amirbekufc1@gmail.com>
2025-07-23 16:07:12 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,51 @@
+// let name = "llllllllllll"
+// console.log(name.length)
+
+// let str = "Hello World"
+// console.log(str.trim());
+
+// console.log(str.length);
+
+// let first = "Umid"
+// let secondd = "boqqorin"
+// let result = first.concat("", secondd);
+// console.log(result);
+
+// let text = "i love jonka"
+// console.log(text.includes ("jonka"))
+// console,log(text. includes ("Python "))
+// alert = prompt ("Ismingizni kiriting")
+// alert = prompt ("Bironta harif kiriting")
+let name = prompt("Ismingizni kiriting");
+let letter = prompt("Ismingizdagi bir harfni kiriting");
+
+let lowerName = name.toLowerCase();
+let lowerLetter = letter.toLowerCase();
+
+if (lowerName.includes(lowerLetter)) {
+  alert(name + " ismingiz ichida " + letter + " harfi mavjud");
+} else {
+  alert(name + " ismingiz ichida " + letter + " harfi mavjud emas");
+}
+
+
+
+
+
+
+
+
+
+// let text = 'Amirbek Maxmudov'
+
+// console.log(text.length)
+// console.log(text.slice(0 ,3))
+// console.log(text.includes('23'))
+// console.log(text.replace("Amirbek","Abdulloh"))
+// console.log(text.trim())
+// console.log(text.toLowerCase())
+// console.log(text.toLocaleUpperCase())
+// console.log(text.split(","))
+
+
+
